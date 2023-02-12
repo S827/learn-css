6cd45913dbf43d531a15f03ed5a7342465917e89
s827 <dskumar827@gmail.com>
2023-02-12 20:10:38 +0530

    Animated accordion with icons

---
+++ How To/Accordion/Accordion with Icons/main.js
@@ -0,0 +1,12 @@
+const acc = document.getElementsByClassName('accordion');
+for(let i = 0; i < acc.length; i++){
+    acc[i].addEventListener('click', () => {
+        acc[i].classList.toggle('active');
+        let panel = acc[i].nextElementSibling;
+        if(panel.style.maxHeight){
+            panel.style.maxHeight = null;
+        } else{
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    } )
+}
