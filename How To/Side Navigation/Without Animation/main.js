a15a0606dcf63b07911cdb89c1027259c5bfdef9
s827 <dskumar827@gmail.com>
2023-02-21 23:36:01 +0530

    overlay Without Animation

---
+++ How To/Side Navigation/Without Animation/main.js
@@ -0,0 +1,8 @@
+const mySidenav = document.getElementById('mySidenav');
+function openNav(){
+    mySidenav.style.display = 'block';
+}
+
+function closeNav(){
+    mySidenav.style.display = 'none';
+}
